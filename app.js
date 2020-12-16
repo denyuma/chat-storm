8f03bc14723f95d6ebab99130589ff9536665f2f
denyuma <yuuma8343@yahoo.co.jp>
2020-12-16 19:04:25 +0900

    gulp log  db insert

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 
+const systemLogger = require('./lib/log/systemLogger.js');
+const accessLogger = require('./lib/log/accessLogger.js');
+
 app.set('view engine', 'pug');
 app.disabled('x-powered-by');
 
-// app.use('/public', express.static(__dirname + '/public/' + (process.env.NODE_ENV === 'development' ? 'development' : 'production')));
+app.use('/public', express.static(__dirname + '/public/' + (process.env.NODE_ENV === 'development' ? 'development' : 'production')));
 
-app.use('/', require('./routes.index.js'));
+app.use(accessLogger());
+
+app.use('/', require('./routes/index.js'));
+
+app.use(systemLogger());
 
 app.listen(8000);
 console.log('listening Port in 8000');
---
+++ config/mongodb.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CONNECTION_URL: 'mongodb://user:user@localhost:27017/chat-storm',
+  DATABASE: 'chat-storm',
+  OPTIONS: {
+    family: 4
+  }
+};
