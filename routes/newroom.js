6a11d2990d82464dc9ae0b25ce6ad518bbdb103a
denyuma <yuuma8343@yahoo.co.jp>
2021-01-01 00:51:08 +0900

    /newroom prg設定

--- routes/newroom.js
+++ routes/newroom.js
@@ -8,6 +8,31 @@ function randomStringGenerator() {
   return require('crypto').randomBytes(4).toString('hex');
 }
 
+router.get('/', (req, res, next) => {
+  const roomId = req.query.roomId;
+  
+  MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
+    const db = client.db(DATABASE);
+
+    db.collection('rooms')
+      .findOne({ roomId: roomId })
+      .then((document) => {
+        const roomName = document.roomName;
+        const roomId = document.roomId;
+        const roomPassword = document.roomPassword;
+        res.render('./newroom.pug', {
+          roomName: roomName,
+          roomId: roomId,
+          roomPassword: roomPassword,
+        });
+      }).catch((error) => {
+        throw error;
+      }).then(() => {
+        client.close();
+      });
+  });
+});
+
 router.post('/', (req, res, next) => {
   const roomName = req.body.roomName || '(部屋名未設定)';
   const roomId = randomStringGenerator();
@@ -15,17 +40,13 @@ router.post('/', (req, res, next) => {
 
   MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
     const db = client.db(DATABASE);
-    
+
     db.collection('rooms').insertOne({
       roomName: roomName,
       roomId: roomId,
       roomPassword: roomPassword
     }).then((doc) => {
-      res.render('./newroom.pug', {
-        roomName: doc.ops[0].roomName,
-        roomId: doc.ops[0].roomId,
-        roomPassword: doc.ops[0].roomPassword
-      });
+      res.redirect(`/newroom?roomId=${roomId}`);
     }).catch((error) => {
       throw error;
     }).then(() => {
@@ -35,5 +56,3 @@ router.post('/', (req, res, next) => {
 });
 
 module.exports = router;
-
-// roomId作成
