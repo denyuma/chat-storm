935d103f5d15e520a46cb09a2e6e196e8fe46f09
denyuma <yuuma8343@yahoo.co.jp>
2021-01-26 23:22:53 +0900

    escape.jsの作成

--- app/entry.js
+++ app/entry.js
@@ -80,15 +80,3 @@ $('#message-send-button').each((i, e) => {
 $messageform.on('submit', (e) => {
   e.preventDefault();
 });
-
-
-//エスケープ処理を行う
-function escape(str){
-  str = str.replace(/&/g, '&amp;');
-  str = str.replace(/>/g, '&gt;');
-  str = str.replace(/</g, '&lt;');
-  str = str.replace(/"/g, '&quot;');
-  str = str.replace(/'/g, '&#x27;');
-  str = str.replace(/`/g, '&#x60;');
-  return str;
-};
---
+++ public/javascripts/functions/escape.js
@@ -0,0 +1,9 @@
+const escape = (str) => {
+  str = str.replace(/&/g, '&amp;');
+  str = str.replace(/>/g, '&gt;');
+  str = str.replace(/</g, '&lt;');
+  str = str.replace(/"/g, '&quot;');
+  str = str.replace(/'/g, '&#x27;');
+  str = str.replace(/`/g, '&#x60;');
+  return str;
+};
